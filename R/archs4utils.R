730096d44d3f295c964592eeffe084668e26546f
Alexander Lachmann <alexander.lachmann@gmail.com>
2018-03-09 14:16:35 -0500

    First commit

---
+++ R/archs4utils.R
@@ -0,0 +1,9 @@
+#' List all versions of ARCHS4.
+#'
+#' @return information of ARCHS4 versions.
+#' @examples
+#' getVersions()
+getVersions <- function(x, y) {
+  print("Hello, world!")
+  res <- "Hello, worlds!"
+}
---
+++ R/hello.R
@@ -0,0 +1,17 @@
+# Hello, world!  This is an example function named 'hello' which prints 'Hello, world!'.  You can learn more about package authoring with RStudio at:
+# http://r-pkgs.had.co.nz/ Some useful keyboard shortcuts for package authoring: Build and Reload Package: 'Cmd + Shift + B' Check Package: 'Cmd +
+# Shift + E' Test Package: 'Cmd + Shift + T'
+
+#' Say hello to the world.
+#'
+#' @param x Dummy variable 1.
+#' @param y Dummy variable 2.
+#' @return The string it printed out.
+#' @examples
+#' hello(1, 1)
+#' hello(10, 1)
+hello <- function(x, y) {
+    print("Hello, world!")
+    res <- "Hello, worlds!"
+}
+
